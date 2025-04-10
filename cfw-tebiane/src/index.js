163ea5d53c4a1c25469aea2fe284a61dbc26854c
mlibre <m.gh@linuxmail.org>
2025-04-10 22:22:25 +0330

    feat: Update echo message and version
    This commit updates the echo message in `src/index.js` to "Echo2" and bumps the package version to 0.0.1 in `package.json`.

--- cfw-tebiane/src/index.js
+++ cfw-tebiane/src/index.js
@@ -66,7 +66,7 @@ async function onUpdate ( update, TOKEN )
  */
 function onMessage ( message, TOKEN )
 {
-	return sendPlainText( message.chat.id, `Echo:\n${ message.text}`, TOKEN );
+	return sendPlainText( message.chat.id, `Echo2:\n${ message.text}`, TOKEN );
 }
 
 /**
